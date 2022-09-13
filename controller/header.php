e3eef82687a9e6d11cbc0eb2eb9565e38539f950
darkesto <104991071+darkesto@users.noreply.github.com>
2022-09-13 16:28:50 +0200

    dark

--- controller/header.php
+++ controller/header.php
@@ -1,4 +1,9 @@
 <?php
 //On affiche le template Twig correspondant
+if (isset($_SESSION['userName'])) {
+    echo $twig->render('header.html.twig',['username' => $_SESSION['email'] ]);
+} else {
+    echo $twig->render('header.html.twig',['username' => 'Pas de utilisateur' ]);
+}
 
-echo $twig->render('header.html.twig',['username' => $_SESSION['userName'] ]);
+
