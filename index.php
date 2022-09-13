cd719aa9561b8c6a47c6d8d0717a2e363bbd3fcf
Guillaume <gjourde@gmail.com>
2022-09-13 09:57:08 +0200

    maj user

--- index.php
+++ index.php
@@ -6,10 +6,14 @@ include './config/config.init.php';
 include _CTRL_ . 'header.php';
 
 // Gestion de Routing
-if (isset($_GET['action']) && file_exists(_CTRL_ . $_GET['action'] . '.php')) {
-    include _CTRL_ . $_GET['action'] . '.php';
-} elseif (isset($_GET['action']) && !file_exists(_CTRL_ . $_GET['action'] . '.php')) {
-    include _CTRL_ . 'erreur.php';
+if (isset($_SESSION["email"])) {
+    if (isset($_GET['action']) && file_exists(_CTRL_ . $_GET['action'] . '.php')) {
+        include _CTRL_ . $_GET['action'] . '.php';
+    } elseif (isset($_GET['action']) && !file_exists(_CTRL_ . $_GET['action'] . '.php')) {
+        include _CTRL_ . 'erreur.php';
+    } else {
+        include _CTRL_ . 'affichageFilms.php';
+    }
 } else {
     include _CTRL_ . 'connexion.php';
 }
