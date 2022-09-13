4e47c6a6c8558549b7efbdb317d276ddfa69d9f6
Kris <62119861+kris86000@users.noreply.github.com>
2022-09-13 16:31:13 +0200

    ajouter film twig a revoir

--- controller/ajouter_film.php
+++ controller/ajouter_film.php
@@ -5,10 +5,10 @@
 $filmsDao = new FilmsDAO();
 if (isset($_POST['titre']) and isset($_POST['realisateur']) and isset($_POST['affiche']) and isset($_POST['annee'])) {
     //On affiche le template Twig correspondant
-    $films = new Films($_POST['idFilm'], $_POST['titre'], $_POST['realisateur'], $_POST['affiche'], $_POST['annee']);
-    $status = $filmsDao->add($films);
+    $film = new Films($_POST['idFilm'], $_POST['titre'], $_POST['realisateur'], $_POST['affiche'], $_POST['annee']);
+    $status = $filmsDao->add($film);
     if ($status) {
-        echo $twig->render('ajouter_film.html.twig', ['status' => "Ajout du Film confirmé ", 'films' => $films]);
+        echo $twig->render('ajouter_film.html.twig', ['status' => "Ajout du Film confirmé ", 'film' => $film]);
     } else {
         echo $twig->render('ajouter_film.html.twig', ['status' => "Erreur Dans l'ajout du Film"]);
     }
