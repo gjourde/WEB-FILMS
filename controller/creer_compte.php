48c1c9f4dfdc2b0d20d92728d18888f84449b54a
Guillaume <gjourde@gmail.com>
2022-09-14 13:29:08 +0200

    connexion

--- controller/creer_compte.php
+++ controller/creer_compte.php
@@ -4,16 +4,18 @@ if (isset($_POST['email']) and isset($_POST['userName']) and isset($_POST['mdp1'
     $userDao = new UserDAO();
     $user = $userDao->getUser($_POST["email"]);
     if ($user->getEmail() == $_POST["email"]) {
-        //Le compte existe déjà
+        echo $twig->render('creer_compte.html.twig', ['status' => "true"]);
     } else {
         //test mdp1 = mdp2
         if ($_POST['mdp1'] != $_POST['mdp2']) {
             //erreur mdp1 != mdp2
         } else {
             //creation
-            $newUser = new User(null, $_POST['userName'], $_POST['email'], $_POST['mdp2']);
+            $newUser = new User(null, $_POST['userName'], $_POST['email'], $_POST['mdp1']);
+            $userDao->add($newUser);
+            //render-> add user ok
         }
     }
 } else {
-    # code...
+    //render-> form
 }
