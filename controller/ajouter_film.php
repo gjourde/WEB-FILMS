98bb58c22357d1bec40038702d4a9232d6600cc4
Kris <62119861+kris86000@users.noreply.github.com>
2022-09-15 08:46:52 +0200

    update secu ajout / supprimer

--- controller/ajouter_film.php
+++ controller/ajouter_film.php
@@ -1,17 +1,20 @@
 <?php
 
 // Controller pour l'ajout d'un nouveau film //
-
-$filmsDao = new FilmsDAO();
-if (isset($_POST['titre']) and isset($_POST['realisateur']) and isset($_POST['affiche']) and isset($_POST['annee'])) {
-    //On affiche le template Twig correspondant
-    $film = new Films($_POST['idFilm'], $_POST['titre'], $_POST['realisateur'], $_POST['affiche'], $_POST['annee']);
-    $status = $filmsDao->add($film);
-    if ($status) {
-        echo $twig->render('ajouter_film.html.twig', ['status' => "Ajout du Film confirmé ", 'films' => $film]);
+if (isset($_SESSION['email'])) {
+    if (isset($_POST['titre']) and isset($_POST['realisateur']) and isset($_POST['affiche']) and isset($_POST['annee'])) {
+        $filmsDao = new FilmsDAO();
+        //On affiche le template Twig correspondant
+        $film = new Films($_POST['idFilm'], $_POST['titre'], $_POST['realisateur'], $_POST['affiche'], $_POST['annee']);
+        $status = $filmsDao->add($film);
+        if ($status) {
+            echo $twig->render('ajouter_film.html.twig', ['status' => "Ajout du Film confirmé ", 'films' => $film]);
+        } else {
+            echo $twig->render('ajouter_film.html.twig', ['status' => "Erreur Dans l'ajout du Film"]);
+        }
     } else {
-        echo $twig->render('ajouter_film.html.twig', ['status' => "Erreur Dans l'ajout du Film"]);
+        echo $twig->render('ajouter_film.html.twig');
     }
 } else {
-    echo $twig->render('ajouter_film.html.twig');
+    header('location:connexion');
 }
